40c35608670c2313bcf57bad1b9990702ba59b7c
tommy <bt@g>
2022-03-10 12:52:39 -0800

    added title changing functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('.header');
+const title = document.querySelector('.title');
 const menu = document.querySelector('.menu');
 const imgWrapper = document.querySelector('.img-wrapper');
 const left = document.querySelector('.left');
@@ -16,30 +17,49 @@ thirdImg.src = './img/bridge.jpg';
 fourthImg = new Image();
 fourthImg.src = './img/sus.jpg';
 
-const imgArray = [firstImg, secondImg, thirdImg, fourthImg];
+// const imgArray = [firstImg, secondImg, thirdImg, fourthImg];
+const imgArray = [{
+  ele: firstImg,
+  title: 'Fruit',
+  desc: 'description 1 goes here',
+}, {
+  ele: secondImg,
+  title: 'Fruit 2',
+  desc: 'description 2 goes here',
+}, {
+  ele: thirdImg,
+  title: 'Bridge',
+  desc: 'description 3 goes here',
+}, {
+  ele: fourthImg,
+  title: '???',
+  desc: 'description 4 goes here',
+}]
 
 let counter = 0;
 
-// append first image from array to DOM
-imgWrapper.appendChild(imgArray[0]); 
-
 function replaceImg() {
   const currIndex = counter % imgArray.length
-  console.log(currIndex);
   imgWrapper.removeChild(imgWrapper.lastChild);
-  imgWrapper.appendChild(imgArray[currIndex]);
+  imgWrapper.appendChild(imgArray[currIndex].ele);
   // I need to add an animation or something around here
   // setTimeout(imgRight, 5900);
 } 
 
+function replaceTitle() {
+  title.textContent = imgArray[counter % imgArray.length].title;
+}
+
 const imgLeft = () => {
   counter === 0 ? counter = imgArray.length - 1 : --counter;
   replaceImg();
+  replaceTitle();
 }
 
 const imgRight = () => {
   ++counter;
   replaceImg();
+  replaceTitle();
 }
 
 left.onclick = imgLeft;
@@ -49,11 +69,9 @@ right.onclick = imgRight;
 genNavDot = (index) => {
   dot = document.createElement('div');
   dot.classList.add('dot');
-
   dot.textContent = index + 1;
-
   dot.onclick = () => {
-    console.log(index);
+    // console.log(index);
     counter = index;
     imgWrapper.removeChild(imgWrapper.lastChild);
     imgWrapper.appendChild(imgArray[index]);
@@ -69,9 +87,14 @@ imgArray.forEach((item, index) => {
 const autoSlide = setInterval(function() {
   ++counter;
   replaceImg();
+  replaceTitle();
 }, 3900); // I want this interval to be reset when a button is clicked
 // because rn it progresses the slide every 3900ms regardless of when last btn was clicked
 
+// append first image from array to DOM
+imgWrapper.appendChild(imgArray[0].ele); 
+// initialize title
+replaceTitle();
 
 
 
